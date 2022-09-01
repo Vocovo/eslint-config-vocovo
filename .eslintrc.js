95f251fb8b70ee4f45698c0f4ccb3f87feebfab9
Pete West <pete.west@vocovo.com>
2022-09-01 15:02:08 +0100

    Lint linting rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,42 +1,39 @@
 const prettierrc = require('./prettierrc')
 
 module.exports = {
-  "extends": ["prettier-standard"],
-  "env": {
-    "jest": true
+  extends: ['prettier-standard'],
+  env: {
+    jest: true,
   },
-  "rules": {
-    "generator-star-spacing": ["warn"],
-    "no-console": [
-      "error",
+  rules: {
+    'generator-star-spacing': ['warn'],
+    'no-console': [
+      'error',
       {
-        "allow": ["warn", "error", "info", "group", "groupEnd"]
-      }
+        allow: ['warn', 'error', 'info', 'group', 'groupEnd'],
+      },
     ],
-    "no-throw-literal": 0,
-    "no-extend-native": 0,
-    "complexity": [
-      "error",
+    'no-throw-literal': 0,
+    'no-extend-native': 0,
+    complexity: [
+      'error',
       {
-        "max": 10
-      }
+        max: 10,
+      },
     ],
-    "func-names": 0,
-    "no-unused-vars": [
+    'func-names': 0,
+    'no-unused-vars': [
       2,
       {
-        "ignoreRestSiblings": true
-      }
+        ignoreRestSiblings: true,
+      },
     ],
-    "standard/no-callback-literal": 0,
-    "no-warning-comments": "warn",
-    "prettier/prettier": [
-      "error",
-      prettierrc
-    ]
+    'standard/no-callback-literal': 0,
+    'no-warning-comments': 'warn',
+    'prettier/prettier': ['error', prettierrc],
   },
-  "globals": {
-    "cy": true,
-    "Cypress": true
-  }
+  globals: {
+    cy: true,
+    Cypress: true,
+  },
 }
